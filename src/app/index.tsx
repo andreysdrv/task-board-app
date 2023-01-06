2279cd80fd8423652b2ff6c3a999b1efdccc38cc
Vladimir Ovchinnikov <metalass21@yandex.ru>
2023-01-06 18:09:52 +0300

    feat: сверстать главную страницу

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,14 @@
 import { Fragment } from 'react'
 import { CssBaseline } from '@mui/material'
+import ButtonAppBar from '../AppBar/AppBar'
+import ButtonBox from '../ButtonBox/ButtonBox'
+import ProjectsBox from '../ProjectsBox/ProjectBox'
 
 export const App = () => (
   <Fragment>
     <CssBaseline />
+    <ButtonAppBar />
+    <ButtonBox />
+    <ProjectsBox />
   </Fragment>
 )
