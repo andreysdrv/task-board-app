05ea0b5e95dcd314c020b4594d40e200460acf30
andreysdrv <andrey-sidorov@russianpost.ru>
2023-01-07 20:31:31 +0300

    ci: fix issue with vite types

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 
+const BASE_NAME = import.meta.env.DEV ? '/' : '/task-board-app'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename={BASE_NAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
